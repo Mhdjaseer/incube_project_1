21e3db990cba25a83d203ed916b02ae3ddea5fd4
jaseer j <jassjaseerdazzils2000@gmail.com>
2023-10-02 16:12:56 +0530

    event image carousal

---
+++ Assets/js/main.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const colOne = document.querySelector(".change-one");
+    const colTwo = document.querySelector(".change-two");
+  
+    colOne.addEventListener("mouseenter", function () {
+      colOne.classList.remove("change-one");
+      colOne.classList.add("swapped-one");
+      colTwo.classList.remove("change-two");
+      colTwo.classList.add("swapped");
+    });
+  
+    colTwo.addEventListener("mouseenter", function () {
+      colOne.classList.remove("swapped-one");
+      colOne.classList.add("change-one");
+      colTwo.classList.remove("swapped");
+      colTwo.classList.add("change-two");
+    });
+  });
+  
